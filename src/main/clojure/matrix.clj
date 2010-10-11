49ab25e632e5ccf92d0d6a66467d9119a7b5ae3b
Milan <Milan@localhost>
2010-10-11 20:58:13 +0200

    Changed indentation. It should now look better on Google Code.

--- src/main/clojure/matrix.clj
+++ src/main/clojure/matrix.clj
@@ -1,34 +1,38 @@
 (ns matrix
-	(:use util))
+  (:use util))
 
 (defn generate-nil-row [size]
-	(ref (vec (repeat size nil))))
+  (ref (vec (repeat size nil))))
 
 (defn generate-nil-matrix [size]
-	(vec (repeatedly size (partial generate-nil-row size))))
+  (vec (repeatedly size (partial generate-nil-row size))))
 	
 (defn get-value-at [matrix i j]
-	(get (deref (get matrix i)) j))
+  (get (deref (get matrix i)) j))
 
 (defn set-value-at [matrix i j v]
-	(dosync (alter (get matrix i) assoc j v))) 
+  (dosync (alter (get matrix i) assoc j v))) 
 	
 (defn generate-initial-matrix [size]
-	(let [matrix (generate-nil-matrix size)]
-				(dotimes [i size]
-					(dotimes [j size]
-						(if (= nil (get-value-at matrix i j))
-							(if (= 0 (- i j))
-								(set-value-at matrix i j 0)
-								(let [v (rand-pos-int 1000)]
-									(do (set-value-at matrix i j v) (set-value-at matrix j i v)))))))
-		matrix))
+  (let [matrix (generate-nil-matrix size)]
+    (dotimes [i size]
+      (dotimes [j size]
+        (if (= nil (get-value-at matrix i j))
+          (if (= 0 (- i j))
+            (set-value-at matrix i j 0)
+            (let [v (rand-pos-int 1000)]
+              (do (set-value-at matrix i j v) (set-value-at matrix j i v)))))))
+  matrix))
 		
 (defn matrix [size]
-	(let [m (generate-initial-matrix size)]
-		(vec (for [row m] @row))))
+  (let [m (generate-initial-matrix size)]
+    (vec (for [row m] @row))))
 		
 (defn get-value [matrix i j]
-	((matrix i) j))
+  ((matrix i) j))
+
+(defn print-matrix [mtx]
+  (dotimes [i (count mtx)]
+    (println (apply str (interpose "	" (mtx i))))))
 	
 	
--- src/main/clojure/util.clj
+++ src/main/clojure/util.clj
@@ -1,10 +1,10 @@
 (ns util)
 
 (defn rand-pos-int 
-	"Returns random positive integer lower than rng. 
-	If rng is not specified result is lower than 100."
-	([] (rand-pos-int 100))
-	([rng] 
-	(let [i (rand-int rng)]
-		(if (> i 0) i
-				(rand-pos-int rng)))))
+  "Returns random positive integer lower than rng. 
+  If rng is not specified result is lower than 100."
+  ([] (rand-pos-int 100))
+  ([rng] 
+  (let [i (rand-int rng)]
+    (if (> i 0) i
+      (rand-pos-int rng)))))
