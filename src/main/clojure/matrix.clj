ef8be2b0408279fe9769085e4dc0436bc8672775
Milan <Milan@localhost>
2010-10-12 18:50:39 +0200

    Documented all functions.

--- src/main/clojure/matrix.clj
+++ src/main/clojure/matrix.clj
@@ -2,19 +2,30 @@
   (:use util))
 
 
-(defn generate-nil-row [size]
+(defn- generate-nil-row 
+  "Generates a vector ref of given size with all its elements being nil."
+  [size]
   (ref (vec (repeat size nil))))
 
-(defn generate-nil-matrix [size]
+(defn- generate-nil-matrix 
+  "Generates square matrix of given dimension with all its elements being nil."
+  [size]
   (vec (repeatedly size (partial generate-nil-row size))))
 	
-(defn get-value-at [matrix i j]
+(defn- get-value-at 
+  "Returns a value in the given matrix with given coordinates."
+  [matrix i j]
   (get (deref (get matrix i)) j))
 
-(defn set-value-at [matrix i j v]
+(defn- set-value-at 
+  "Sets value at the given matrix at given coordinates."
+  [matrix i j v]
   (dosync (alter (get matrix i) assoc j v))) 
 	
-(defn generate-initial-matrix [size]
+(defn- generate-initial-matrix 
+  "Generates random symmetric matrix of given dimension with 0s on its diagonal.
+  Rows in matrix are represented as vector refs."
+  [size]
   (let [matrix (generate-nil-matrix size)]
     (dotimes [i size]
       (dotimes [j size]
@@ -25,14 +36,21 @@
               (do (set-value-at matrix i j v) (set-value-at matrix j i v)))))))
   matrix))
 		
-(defn create-matrix [size]
+(defn create-matrix 
+  "Generates random symmetric matrix of given dimension with 0s on its diagonal.
+  Matrix is represented as a vector of vectors."
+  [size]
   (let [m (generate-initial-matrix size)]
     (vec (for [row m] @row))))
 		
-(defn get-value [matrix i j]
+(defn get-value 
+  "Returns the value in the matrix at given position."
+  [matrix i j]
   ((matrix i) j))
 
-(defn print-matrix [mtx]
+(defn print-matrix 
+  "Prints a matrix that is represented as a vector of vectors."
+  [mtx]
   (dotimes [i (count mtx)]
     (println (apply str (interpose "	" (mtx i))))))
 	
--- src/main/clojure/tsp.clj
+++ src/main/clojure/tsp.clj
@@ -3,42 +3,62 @@
   (:use util))
 
 
-(defn create-single-chromosome [size]
+(defn create-single-chromosome 
+  "Creates random vector of given size that represent single salesman path."
+  [size]
   (vec (shuffle (range size))))
 	
-(defn generate-initial-population [pop-size chrom-size]
+(defn generate-initial-population 
+  "Generates population of given size that contains random salesman paths.
+  Salesman path is a vector of ints beetwen 0 and chrom-size."
+  [pop-size chrom-size]
   (repeatedly pop-size (partial create-single-chromosome chrom-size)))
   
-(defn calculate-distance [mtx salesman-path]
+(defn calculate-distance 
+  "Calculates the distance for given path based on the values in matrix."
+  [mtx salesman-path]
   (loop [sum 0 p salesman-path]
     (if (> (count p) 1) 
       (recur (+ sum (get-value mtx (first p) (second p))) (rest p))
       (+ sum (get-value mtx (last salesman-path) (first salesman-path))))))
       
-(defn sort-by-distance [mtx population]
+(defn sort-by-distance 
+  "Sorts paths in the population by minimal distance."
+  [mtx population]
   (sort-by (partial calculate-distance mtx) population))
   
-(defn mutate [chromosome]
+(defn mutate 
+  "Swaps two random elements in the chromosome."
+  [chromosome]
   (let [chrom-size (count chromosome)]
     (swap chromosome (rand-int chrom-size) (rand-int chrom-size))))
 	
-(defn crossover [parent1 parent2 mtx]
+(defn crossover 
+  "Returns new offspring only if its distance is lower than first parent's distance.
+  Otherwise it returns first parent (because it always has lower distance than second parent)."
+  [parent1 parent2 mtx]
   (let [idx (/ (count parent1) 2)
         p1 (subvec parent1 0 idx)
         child (reduce insert-if-different p1 parent2)]
     (if (< (calculate-distance mtx child) (calculate-distance mtx parent1)) child parent1)))
     
-(defn next-generation [population m]
+(defn next-generation 
+  "Generates new generation using crossover."
+  [population m]
   (loop [res (lazy-seq) p population]
     (if (> (count p) 1)
       (recur (conj res (crossover (first p) (second p) m)) (rest p))
       (conj res (crossover (first population) (last population) m)))))
 			
-(defn next-mutated-gen [population m]
+(defn next-mutated-gen 
+  "Generates new generation using crossover and mutation."
+  [population m]
   (let [p (next-generation population m)]
     (interleave (map mutate p) p)))
 
-(defn solve [mtx population generation no-prosperity-for best-path]
+(defn- solve 
+  "Used by solve-tsp function to solve the TSP problem."
+  [mtx population generation no-prosperity-for best-path]
   (let [sorted-population (sort-by-distance mtx population)
         current-best-path (first sorted-population)
         current-best-path-val (calculate-distance mtx current-best-path)
@@ -47,11 +67,14 @@
       (if (< current-best-path-val best-path-val) 
         {:cost current-best-path-val, :path current-best-path, :generation generation} 
         {:cost best-path-val, :path best-path, :generation generation})
-      (solve mtx (next-mutated-gen (half-of sorted-population) mtx) (inc generation) 
+      (recur mtx (next-mutated-gen (half-of sorted-population) mtx) (inc generation) 
         (if (< current-best-path-val best-path-val) 0 (inc no-prosperity-for)) 
         (if (< current-best-path-val best-path-val) current-best-path best-path)))))
 
-(defn solve-tsp [mtx]
+(defn solve-tsp 
+  "Solves the TSP problem for given matrix.
+  The result is a map that contains keys :cost, :generation and :path."
+  [mtx]
   (let [initial-population (generate-initial-population 500 (count mtx))]
     (solve mtx initial-population 0 0 (create-single-chromosome (count mtx)))))
 
